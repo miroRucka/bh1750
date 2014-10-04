bb1840e0ea8cc7e3aa850f1807344b351ca91044
rucka <miroslav.rucka@posam.sk>
2014-10-04 21:22:23 +0200

    add simple sample

--- example/index.js
+++ example/index.js
@@ -1,5 +1,9 @@
-var BH1750 = require('./bh1750');
+var BH1750 = require('../bh1750');
 var light = new BH1750();
+
+light.readLight(function(value){
+    console.log("light value is: ", value);
+});
 
 
 
